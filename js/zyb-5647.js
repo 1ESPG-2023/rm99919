72544d077a2b9791444c56b06fa5582bca73bd06
FernandoPLP <rm99919@fiap.com.br>
2023-04-18 11:04:25 -0300

    criando pasta css e img

--- js/zyb-5647.js
+++ js/zyb-5647.js
@@ -26,28 +26,30 @@
 //     })
 // })
 
-//declaração de arrays
-let nr1 = [1, 2, 3, 4, 5]
-let nr2 = [ 6, 7, 8, 9, 10]
-//espalhar o array com[...]
-let nr3 = [...nr1,... nr2]
-console.log(`Array-1 ${nr1}`);
-console.log(`Array-2 ${nr2}`);
-console.log(`Array-3 ${nr3}`);
+// //declaração de arrays
+// let nr1 = [1, 2, 3, 4, 5]
+// let nr2 = [ 6, 7, 8, 9, 10]
+// //espalhar o array com[...]
+// let nr3 = [...nr1,... nr2]
+// console.log(`Array-1 ${nr1}`);
+// console.log(`Array-2 ${nr2}`);
+// console.log(`Array-3 ${nr3}`);
 
-nr3.map((numero, key)=>{
-    console.log(`${key+1} - Itens do array - ${numero}`)
+// nr3.map((numero, key)=>{
+//     console.log(`${key+1} - Itens do array - ${numero}`)
   
-})
+// })
 
-//convertendo htmllCollection em array de forma indireta
-const liELementHTML = document.getElementsByTagName("li");
-const liELementArray = [...liELementHTML];
-liELementArray.map((item,key)=>{
-    if (item.textContent == "item-39"){
-        console.log(`${key+1}-${item.textContent}`)
-    }  
-})
+// //convertendo htmllCollection em array de forma indireta
+// const liELementHTML = document.getElementsByTagName("li");
+// const liELementArray = [...liELementHTML];
+// liELementArray.map((item,key)=>{
+//     if (item.textContent == "item-39"){
+//         console.log(`${key+1}-${item.textContent = "LOCALIZADO"}`)
+//     }  
+// })
+
+
 
 
 
