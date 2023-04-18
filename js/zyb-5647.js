f8da3e0867b946af55bdbda4aefac7f97de441b4
FernandoPLP <rm99919@fiap.com.br>
2023-04-18 10:50:42 -0300

    htmllCollection em array de forma indireta

--- js/zyb-5647.js
+++ js/zyb-5647.js
@@ -1,18 +1,51 @@
-const liELement = document.getElementsByTagName("li");
+//const liELement = document.getElementsByTagName("li");
 //console.log(liELement)
 //for (let index = 0; index < liELement.length; index++) {
 //    console.log(liELement[index].textContent)
 //}
 
 //declaração de arrays
-let nr1 = [1,2,3,4,5];
-let nr2 = [6,7,8,9,10];
-let nr3 = nr1+nr2
-console.log(`Array-1 ${nr1}`)
-console.log(`array -2 ${nr2}`)
-console.log(`Array-3 ${nr3}`)
+// let nr1 = [1, 2, 3, 4, 5]
+// let nr2 = [, 6, 7, 8, 9, 10]
+// let nr3 = [nr1, nr2]
+// console.log(`Array-1 ${nr1}`);
+// console.log(`Array-2 ${nr2}`);
+// console.log(`Array-3 ${nr3}`);
+   
+//    // listando um Array
+// nr3.forEach((itensDoArray) => {
+//     console.log(`Array-3 sendo listado ${itensDoArray}`)
+// });
 
-nr3.forEach((itemDoArray)=>{
-    console.log(`Array-3 sendo listado ${itemDoArray}`)
-});
-   console.log(`Array-3 sendo listado ${nr3}`)
+// //listando o array com MAP
+// nr3.map((numero, key)=>{
+
+//     console.log(`${key+1} - Itens do array - ${numero}`)
+//     numero.forEach((digitos)=>{
+//         console.log(digitos)
+//     })
+// })
+
+//declaração de arrays
+let nr1 = [1, 2, 3, 4, 5]
+let nr2 = [ 6, 7, 8, 9, 10]
+//espalhar o array com[...]
+let nr3 = [...nr1,... nr2]
+console.log(`Array-1 ${nr1}`);
+console.log(`Array-2 ${nr2}`);
+console.log(`Array-3 ${nr3}`);
+
+nr3.map((numero, key)=>{
+    console.log(`${key+1} - Itens do array - ${numero}`)
+  
+})
+
+//convertendo htmllCollection em array de forma indireta
+const liELementHTML = document.getElementsByTagName("li");
+const liELementArray = [...liELementHTML];
+liELementArray.map((item,key)=>{
+    console.log(`${key+1}-${item.textContent}`)
+})
+console.log(liELementArray);
+
+
